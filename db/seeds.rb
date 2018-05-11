2229c0d979eae5722b09f3172c871f341011e1b3
charles-henri poniard <c.poniard@gmail.com>
2018-05-11 12:13:11 +0200

    Add figaro - Protect my secret data in config/application.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
+puts "Clean data base"
+Ingredient.delete_all
+
+
+
+
+puts "Create data base"
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Evian")
+Ingredient.create(name: "CocaCola")
+Ingredient.create(name: "Whisky")
+Ingredient.create(name: "Bourbon")
+Ingredient.create(name: "Rhum")
+Ingredient.create(name: "Cognac")
+
+puts "data base clear"
+
